4db0225f1541fd46f897a637c957eeda9ae593dd
Bo Sun <bosun58@gmail.com>
2015-05-17 21:51:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HomeTheatre::Application.config.secret_key_base = '8663bf8618d146958a8d64a42c98fb730b5a8cd1832e95cb5a3f830faa4421f22b7ac18402c94148291c5147677b5358299a10d677ca3432996d9c3af137faf7'
